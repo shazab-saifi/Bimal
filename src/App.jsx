a7083857fd079596e86fbb4a80bdd881f86dd2cb
shazab-saifi <shazebsaifi92@gmail.com>
2024-10-29 16:30:45 +0530

    Fixed a bug in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer"
 function App() {
 
   return (
-    <div className="w-full min-h-screen custom-gradient flex flex-col items-center dm-sans">
+    <div className="w-screen min-h-screen custom-gradient flex flex-col items-center dm-sans">
       <Header />
       <Hero />
       <Footer/>
