099e7ac3eaf41ab4308dec3f369e955a04999eb9
shazab-saifi <shazebsaifi92@gmail.com>
2024-10-31 09:08:11 +0530

    FIxed few bugs in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 const Header = () => {
   return (
-    <div className='h-auto md:h-auto w-full xl:w-[1440px] flex justify-between py-5 px-4 md:py-6 md:px-16 lg:px-[100px] box-border'>
+    <div className='h-auto w-full max-w-[1440px] mx-auto flex justify-between py-5 px-4 md:py-6 md:px-16 lg:px-24 box-border'>
         <h1 className='text-kesari-500 text-2xl leading-[28.8px] tracking-tight text-center font-extrabold'>Bimal</h1>
         <div className='flex gap-4'>
             <h4 className='text-kesari-900 text-[16px] leading-5 font-medium tracking-[-0.01em] text-center flex items-center'>Products</h4>
